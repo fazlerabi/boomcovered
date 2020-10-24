763d00a54970fc5aa7fab09e1384bd5f3a5b7787
luck21honey <lukeadamdev@gmail.com>
2020-10-24 09:02:59 -0400

    footer work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,20 @@
-import {AfterViewInit, Component, HostListener} from '@angular/core';
-import {NavigationEnd, NavigationStart, Router} from '@angular/router';
-import {Location} from '@angular/common';
+import { AfterViewInit, Component, HostListener } from "@angular/core";
+import { NavigationEnd, NavigationStart, Router } from "@angular/router";
+import { Location } from "@angular/common";
 
-import {filter} from 'rxjs/operators';
-import {slideInAnimation} from './route-animation';
-import {CommonService} from './services/common.service';
-import {LocalStorageService} from 'angular-web-storage';
+import { filter } from "rxjs/operators";
+import { slideInAnimation } from "./route-animation";
+import { CommonService } from "./services/common.service";
+import { LocalStorageService } from "angular-web-storage";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  animations: [slideInAnimation]
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
+  animations: [slideInAnimation],
 })
 export class AppComponent implements AfterViewInit {
-  title = 'app';
+  title = "app";
   isProcessing = true;
   private _previousUrl: string;
   private _currentUrl: string;
@@ -22,59 +22,51 @@ export class AppComponent implements AfterViewInit {
   isShowFooter = false;
   public navColor: string;
 
-  public constructor(
-    public router: Router,
-    public location: Location,
-    public commonService: CommonService,
-    private local: LocalStorageService) {
+  public constructor(public router: Router, public location: Location, public commonService: CommonService, private local: LocalStorageService) {
     this._routeHistory = [];
 
-    this.router.events.pipe(
-      filter(event => event instanceof NavigationStart)
-    ).subscribe((event) => {
+    this.router.events.pipe(filter((event) => event instanceof NavigationStart)).subscribe((event) => {
       this.isShowFooter = false;
     });
 
-    this.router.events.pipe(
-      filter(event => event instanceof NavigationEnd)
-    ).subscribe((event) => {
-      switch (event['url']) {
-        case '/':
+    this.router.events.pipe(filter((event) => event instanceof NavigationEnd)).subscribe((event) => {
+      console.log("aa: ", event["url"]);
+      switch (event["url"]) {
+        case "/":
           this.fitUI(1000);
           break;
-        case '/step1':
+        case "/step1":
           this.fitUI(1000);
           break;
-        case '/step2':
+        case "/step2":
           this.fitUI(1000);
           break;
-        case '/step3':
+        case "/step3":
           this.fitUI(1000);
           break;
-        case '/roof':
+        case "/roof":
           this.fitUI(1000);
           break;
-        case '/exterior':
+        case "/exterior":
           this.fitUI(1000);
           break;
-        case '/step4':
+        case "/step4":
           this.fitUI(1000);
           break;
-        case '/step5':
+        case "/step5":
           this.fitUI(1000);
           break;
-        case '/step6':
+        case "/step6":
           this.fitUI(1000);
           break;
-        case '/step7':
+        case "/step7":
           this.fitUI(1000);
           break;
-        case '/step8':
+        case "/step8":
           this.fitUI(1000);
           break;
-        case '/step9':
-          (<HTMLElement>document.getElementsByClassName('red').item(0)).style.cssText =
-            'background-color:#0644f4 !important;';
+        case "/step9":
+          (<HTMLElement>document.getElementsByClassName("red").item(0)).style.cssText = "background-color:#0644f4 !important;";
 
           this.fitUI(1000);
           break;
@@ -82,60 +74,53 @@ export class AppComponent implements AfterViewInit {
       this.applyHeaderColor();
       window.scrollTo(0, 0);
     });
-    this.router.events
-      .pipe(filter(event => event instanceof NavigationEnd))
-      .subscribe((event: NavigationEnd) => {
-        this._setURLs(event);
-      });
+    this.router.events.pipe(filter((event) => event instanceof NavigationEnd)).subscribe((event: NavigationEnd) => {
+      this._setURLs(event);
+    });
   }
 
-  @HostListener('window:resize')
-
+  @HostListener("window:resize")
   onResize() {
     this.applyHeaderColor();
   }
 
-  @HostListener('window:scroll')
+  @HostListener("window:scroll")
   onScroll() {
     this.applyHeaderColor();
   }
   applyHeaderColor() {
-    const navbar = <HTMLElement>document.querySelector('nav');
-    const isHeaderCollapse = navbar.classList.contains('top-nav-collapse');
-    let marginToNav, togglerColor = '255, 255, 255, 1';
-    if (isHeaderCollapse || (this.router.url !== '/' && this.router.url !== '/index')) {
-      (<HTMLElement>document.getElementsByClassName('navbar').item(0)).style.cssText =
-        'box-shadow:none';
-      this.navColor = 'black';
-      togglerColor = '0, 0, 0, 1';
-      navbar.style.backgroundColor = 'white';
-      this.navColor = '#ed1c24';
+    const navbar = <HTMLElement>document.querySelector("nav");
+    const isHeaderCollapse = navbar.classList.contains("top-nav-collapse");
+    let marginToNav,
+      togglerColor = "255, 255, 255, 1";
+    if (isHeaderCollapse || (this.router.url !== "/" && this.router.url !== "/index")) {
+      (<HTMLElement>document.getElementsByClassName("navbar").item(0)).style.cssText = "box-shadow:none";
+      this.navColor = "black";
+      togglerColor = "0, 0, 0, 1";
+      navbar.style.backgroundColor = "white";
+      this.navColor = "#ed1c24";
     } else {
-      navbar.style.boxShadow = 'none';
-      navbar.style.backgroundColor = 'transparent';
-      this.navColor = 'black';
-      togglerColor = '255, 255, 255, 1';
-
+      navbar.style.boxShadow = "none";
+      navbar.style.backgroundColor = "transparent";
+      this.navColor = "black";
+      togglerColor = "255, 255, 255, 1";
     }
 
-    if (!(this.router.url === '/' || this.router.url === '/index')) {
-
-      togglerColor = '0,0,0, 1';
-      marginToNav = '60px';
-      navbar.style.backgroundColor = 'white';
-      this.navColor = 'black';
+    if (!(this.router.url === "/" || this.router.url === "/index")) {
+      togglerColor = "0,0,0, 1";
+      marginToNav = "60px";
+      navbar.style.backgroundColor = "white";
+      this.navColor = "black";
     } else {
-      marginToNav = '0px';
+      marginToNav = "0px";
     }
-    togglerColor = '244, 60, 88';
-    this.navColor = '244, 60, 88';
-    (<HTMLElement>document.getElementsByClassName('navbar-toggler-icon')[0]).style.cssText =
-      'background-image: url(\'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30">' +
-      '<path stroke="rgba(' + togglerColor + ')" stroke-width="4" stroke-linecap="round" stroke-miterlimit="10" ' +
-      'd="M4 7h22M4 15h22M4 23h22"/></svg>\') !important;';
+    togglerColor = "244, 60, 88";
+    this.navColor = "244, 60, 88";
+    (<HTMLElement>document.getElementsByClassName("navbar-toggler-icon")[0]).style.cssText =
+      'background-image: url(\'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30">' + '<path stroke="rgba(' + togglerColor + ')" stroke-width="4" stroke-linecap="round" stroke-miterlimit="10" ' + 'd="M4 7h22M4 15h22M4 23h22"/></svg>\') !important;';
 
     setTimeout(() => {
-      const activedLink = (<HTMLElement>document.querySelector('.nav-item.active > a'));
+      const activedLink = <HTMLElement>document.querySelector(".nav-item.active > a");
       // if (activedLink) activedLink.style.borderBottom = "solid 2px " + this.navColor;
     }, 500);
   }
@@ -168,6 +153,4 @@ export class AppComponent implements AfterViewInit {
   get routeHistory(): string[] {
     return this._routeHistory;
   }
-
 }
-
